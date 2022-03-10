8f3da1ca098abd2962a7a40fa780a925ea1fd35e
tongjieee <82252150+tongjieee@users.noreply.github.com>
2022-03-10 09:48:41 +0800

    Update first_game.py

--- first_game.py
+++ first_game.py
@@ -6,19 +6,19 @@ answer = random.randint(1, 10)
 count = 3
 while count > 0:
 
-    temp = input("不妨猜一下小甲鱼现在心里想的是哪个数字：")
+    temp = input("不妨猜一下我现在心里想的是哪个数字：")
     guess = int (temp)
 
     if guess == answer:
-        print("你是小甲鱼心里的蛔虫嘛？！")
+        print("你是我心里的蛔虫嘛？！")
         print("哼，猜中了也没奖励！")
         break
     else:
         if guess < answer:
-            print("too small~")
+            print("太小咯~")
         else:
-            print("too big~")
+            print("太大咯~")
     count = count - 1
 
 
-print("游戏结束，不玩啦……")
+print("游戏结束，不玩啦！")
